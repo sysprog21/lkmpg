2db805da2c1b0deb1c5693f80df8c06dd91bc544
Jim Huang <jserv.tw@gmail.com>
2021-08-17 13:07:06 +0800

    Check coding style in CI

---
+++ .ci/check-format.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SOURCES=$(find $(git rev-parse --show-toplevel) | egrep "\.(cpp|cc|c|h)\$")
+
+CLANG_FORMAT=$(which clang-format-11)
+if [ $? -ne 0 ]; then
+    CLANG_FORMAT=$(which clang-format)
+    if [ $? -ne 0 ]; then
+        echo "[!] clang-format not installed. Unable to check source file format policy." >&2
+        exit 1
+    fi
+fi
+
+set -x
+
+for file in ${SOURCES};
+do
+    $CLANG_FORMAT ${file} > expected-format
+    diff -u -p --label="${file}" --label="expected coding style" ${file} expected-format
+done
+exit $($CLANG_FORMAT --output-replacements-xml ${SOURCES} | egrep -c "</replacement>")
