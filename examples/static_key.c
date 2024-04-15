e1b44579b0f416a20cfe7b9ba0205e1fb674dba5
Jim Huang <jserv@ccns.ncku.edu.tw>
2024-04-16 06:24:06 +0800

    Ensure Linux v6.5 compatibility

--- examples/static_key.c
+++ examples/static_key.c
@@ -61,7 +61,11 @@ static int __init chardev_init(void)
 
     pr_info("I was assigned major number %d\n", major);
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 4, 0)
     cls = class_create(THIS_MODULE, DEVICE_NAME);
+#else
+    cls = class_create(DEVICE_NAME);
+#endif
 
     device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
 
