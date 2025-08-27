2c88f74d7c268b6bc405fbfd9afec26a4095e09a
Jordan Chiu <jordan871130@gmail.com>
2025-08-28 00:37:09 +0800

    Fix incorrect array size in gpio_free_array call
    The gpio_free_array function was incorrectly passed ARRAY_SIZE(leds)
    when freeing the 'buttons' array in multiple examples:
    - examples/intrp.c
    - examples/bottomhalf.c
    - examples/bh_halfthreaded.c
    
    This mismatch could lead to invalid memory access if the size of
    'buttons' differs from 'leds'.
    
    Updated all occurrences to use ARRAY_SIZE(buttons) for correctness.
    
    Co-authored-by: EricccTaiwan <yphbchou0911@gmail.com>
    Signed-off-by: Jordan Chiu <jordan871130@gmail.com>

--- examples/bh_threaded.c
+++ examples/bh_threaded.c
@@ -171,7 +171,7 @@ fail3:
     free_irq(button_irqs[0], &buttons[0]);
 
 fail2:
-    gpio_free_array(buttons, ARRAY_SIZE(leds));
+    gpio_free_array(buttons, ARRAY_SIZE(buttons));
 
 fail1:
     gpio_free_array(leds, ARRAY_SIZE(leds));
